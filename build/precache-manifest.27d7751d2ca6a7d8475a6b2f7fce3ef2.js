3419effb5d9dc437e75fcd63f067ce4f2dea69e8
cnr1724 <42274558+cnr1724@users.noreply.github.com>
2020-08-13 15:49:33 +0300

    Corona Status Tracker
    Corona Status Tracker

---
+++ build/precache-manifest.27d7751d2ca6a7d8475a6b2f7fce3ef2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9789cd086e2321e3828e0a218b75a69a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d7e57b5254fba8a7b3c",
+    "url": "/static/css/main.88fad304.chunk.css"
+  },
+  {
+    "revision": "6d19d1ac3fcba2b755e2",
+    "url": "/static/js/2.bcbc072d.chunk.js"
+  },
+  {
+    "revision": "9874c5c915a0b080707ff2f326f718c1",
+    "url": "/static/js/2.bcbc072d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d7e57b5254fba8a7b3c",
+    "url": "/static/js/main.022a0db0.chunk.js"
+  },
+  {
+    "revision": "94c39df6648a20eea487",
+    "url": "/static/js/runtime-main.9a861b65.js"
+  },
+  {
+    "revision": "9ddbdbc60ac4fa4b7c87070e4881bff2",
+    "url": "/static/media/image.9ddbdbc6.png"
+  }
+]);
